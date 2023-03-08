0eb2f0f2f479a8a776a261b853b5b9caae363dd1
Nikhil Kumar <nk748577732@gmail.com>
2023-03-08 23:10:12 +0530

    Almost finished mobile layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
+const ham = document.querySelector('.hamburger');
+const nav = document.querySelector('.navv');
+const body = document.querySelector('body');
 
+ham.addEventListener('click', () => {
+    ham.classList.toggle('active');
+    body.classList.toggle('active');
+    nav.classList.toggle('active');
+
+});
