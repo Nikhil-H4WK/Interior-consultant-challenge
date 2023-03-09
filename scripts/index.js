75f8309ef0fccb449c496074d2d40f423095838f
Nikhil Kumar <nk748577732@gmail.com>
2023-03-09 11:59:23 +0530

    Finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
 const ham = document.querySelector('.hamburger');
 const nav = document.querySelector('.navv');
 const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+const mainContent = document.querySelector('main');
 
 ham.addEventListener('click', () => {
     ham.classList.toggle('active');
     body.classList.toggle('active');
     nav.classList.toggle('active');
+
 
 });
